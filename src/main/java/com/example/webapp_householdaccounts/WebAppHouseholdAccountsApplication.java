ac35fe156a970261be61e6424898a4ee9dc654b2
showayamada <yamasho082433@gmail.com>
2023-07-04 11:34:11 +0900

    first commit

---
+++ src/main/java/com/example/webapp_householdaccounts/WebAppHouseholdAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp_householdaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppHouseholdAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppHouseholdAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webapp_householdaccounts/WebAppHouseholdAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapp_householdaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppHouseholdAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
